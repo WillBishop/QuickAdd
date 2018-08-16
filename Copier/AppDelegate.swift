0b9127f598c19b4a0eb9468c2bec36af70e72be5
Will Bishop <willbishop@w-macbook-pro.local>
2018-08-16 20:15:18 +0930

    Initial Commit

---
+++ Copier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Copier
+//
+//  Created by Will Bishop on 16/8/18.
+//  Copyright © 2018 Will Bishop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Copier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Copier
+//
+//  Created by Will Bishop on 16/8/18.
+//  Copyright © 2018 Will Bishop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
